14a67c290b4948b0ae957514bf491360fa6e2f84
javlintor <jlinarestorres2@gmail.com>
2025-01-09 10:27:16 +0100

    remove windows opt in extend-mini-files.lua

--- lua/plugins/extend-mini-files.lua
+++ lua/plugins/extend-mini-files.lua
@@ -24,11 +24,6 @@ return {
     },
   },
   opts = {
-    windows = {
-      width_nofocus = 20,
-      width_focus = 50,
-      width_preview = 100,
-    },
     options = {
       use_as_default_explorer = true,
     },
