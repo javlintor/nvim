446193367d089f3395c0898260866beb1ce10a09
javlintor <jlinarestorres2@gmail.com>
2025-02-21 10:04:54 +0100

    remove config

--- lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
--- -- Map <Esc><Esc> to exit terminal mode
-vim.api.nvim_set_keymap("t", "<Esc><Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
--- lua/plugins/venv-selector.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "linux-cultist/venv-selector.nvim",
-  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-  event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-  keys = {
-    -- Keymap to open VenvSelector to pick a venv.
-    { "<leader>cv", "<cmd>VenvSelect<cr>" },
-  },
-}
