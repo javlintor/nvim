12131dcf7e5fc84842a1412d75fc47eb6948b9ee
Javier Linares <jlinarestorres2@gmail.com>
2025-02-28 23:02:39 +0100

    telescopeeeee

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,14 +1,40 @@
 return {
 	'nvim-telescope/telescope.nvim',
 	tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+	},
 	config = function()
+		require("telescope").setup {
+			pickers = {
+				find_files = {
+					theme = "ivy"
+				},
+			},
+			extensions = {
+				fzf = {}
+			}
+		}
+		require("telescope").load_extension('fzf')
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 		vim.keymap.set(
 			'n',
 			'<leader>en',
-			builtin.find_files,
+			function()
+				builtin.find_files { cwd = vim.fn.stdpath("config") }
+			end,
+			{ desc = 'Edit Neovim' }
+		)
+		vim.keymap.set(
+			'n',
+			'<leader>en',
+			function()
+				builtin.find_files({
+					cwd = vim.fn.stdpath("config")
+				})
+			end,
 			{ desc = 'Edit Neovim' }
 		)
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
