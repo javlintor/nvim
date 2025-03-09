78addab2a3fafc4863bdffc29467db3fc24cbfab
Javier Linares <jlinarestorres2@gmail.com>
2025-03-09 20:16:06 +0100

    trouble plugin, set keymaps to <leader>td and <leader>tc 🔎

---
+++ lua/config/plugins/trouble.lua
@@ -0,0 +1,22 @@
+return {
+	"folke/trouble.nvim",
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>td",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>tq",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+		{
+			"<leader>ts",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+	},
+}
