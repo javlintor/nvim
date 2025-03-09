d3210f4720326bbc950cfc30b77a9529ade18d3c
Javier Linares <jlinarestorres2@gmail.com>
2025-03-09 20:14:55 +0100

    barbar config

---
+++ lua/config/plugins/barbar.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		'romgrk/barbar.nvim',
+		dependencies = {
+			'lewis6991/gitsigns.nvim',  -- OPTIONAL: for git status
+			'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+		},
+		init = function()
+			vim.g.barbar_auto_setup = false
+			vim.keymap.set("n", "H", "<cmd>BufferPrevious<CR>")
+			vim.keymap.set("n", "L", "<cmd>BufferPrevious<CR>")
+		end,
+		opts = {
+		},
+		version = '^1.0.0',
+	}
+}
