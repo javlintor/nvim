9039a7923fee95db7f1af56c4e5eefb387607b3c
javlintor <jlinarestorres2@gmail.com>
2025-03-12 16:10:20 +0100

    change theme to onedark

--- lua/config/plugins/catppuccin.lua
+++ lua/config/plugins/catppuccin.lua
@@ -1,7 +1,15 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	config = function()
-		vim.cmd.colorscheme "catppuccin"
-	end
+	-- {
+	-- 	"catppuccin/nvim",
+	-- 	name = "catppuccin",
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme "catppuccin"
+	-- 	end
+	-- }
+	{
+		"navarasu/onedark.nvim",
+		config = function()
+			require("onedark").load()
+		end
+	}
 }
