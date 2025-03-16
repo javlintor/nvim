69f341dc38f9be91265cd436cef55e775548c9d7
Javier Linares <jlinarestorres2@gmail.com>
2025-03-16 19:43:47 +0100

    neotree config

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -20,6 +20,12 @@ return {
           ['\\'] = 'close_window',
         },
       },
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignore = false,
+        hide_by_name = { '.git' },
+      },
     },
   },
 }
