d172b4a6825d99bda5e8a37f21771a89e8c95faf
javlintor <jlinarestorres2@gmail.com>
2025-04-25 15:54:21 +0200

    redefined init.lua

--- init.lua
+++ init.lua
@@ -190,6 +190,8 @@ vim.api.nvim_create_autocmd('BufEnter', {
     if vim.bo.buftype == '' then
       vim.wo.number = true
       vim.wo.relativenumber = true -- Optional
+      vim.opt.scroll = 5
+      vim.opt.scrolloff = 10
     end
   end,
 })
@@ -425,7 +427,7 @@ require('lazy').setup({
           find_files = { hidden = true },
           live_grep = {
             additional_args = function()
-              return { '--hidden' }
+              return { '--hidden', '--smart-case' }
             end,
           },
         },
@@ -1040,13 +1042,12 @@ require('lazy').setup({
       require('onedark').load()
     end,
   },
-
-  -- {
-  --   'stevanmilic/nvim-lspimport',
-  --   config = function()
-  --     vim.keymap.set('n', '<leader>a', require('lspimport').import, { noremap = true })
-  --   end,
-  -- },
+  {
+    'stevanmilic/nvim-lspimport',
+    config = function()
+      vim.keymap.set('n', '<leader>a', require('lspimport').import, { noremap = true })
+    end,
+  },
   {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
@@ -1074,6 +1075,117 @@ require('lazy').setup({
   },
   {
     'mfussenegger/nvim-dap',
+    dependencies = {
+      -- Creates a beautiful debugger UI
+      'rcarriga/nvim-dap-ui',
+      -- Required dependency for nvim-dap-ui
+      'nvim-neotest/nvim-nio',
+      -- Installs the debug adapters for you
+      'williamboman/mason.nvim',
+    },
+    keys = {
+      -- Basic debugging keymaps, feel free to change to your liking!
+      {
+        '<F5>',
+        function()
+          require('dap').continue()
+        end,
+        desc = 'Debug: Start/Continue',
+      },
+      {
+        '<leader>bb',
+        function()
+          require('dap').continue()
+        end,
+        desc = 'Debug: Start/Continue',
+      },
+      {
+        '<F1>',
+        function()
+          require('dap').step_into()
+        end,
+        desc = 'Debug: Step Into',
+      },
+      {
+        '<F2>',
+        function()
+          require('dap').step_over()
+        end,
+        desc = 'Debug: Step Over',
+      },
+      {
+        '<F3>',
+        function()
+          require('dap').step_out()
+        end,
+        desc = 'Debug: Step Out',
+      },
+      {
+        '<leader>b',
+        function()
+          require('dap').toggle_breakpoint()
+        end,
+        desc = 'Debug: Toggle Breakpoint',
+      },
+      {
+        '<leader>B',
+        function()
+          require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+        end,
+        desc = 'Debug: Set Breakpoint',
+      },
+      -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+      {
+        '<F7>',
+        function()
+          require('dapui').toggle()
+        end,
+        desc = 'Debug: See last session result.',
+      },
+    },
+    config = function()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+      -- Dap UI setup
+      -- For more information, see |:help nvim-dap-ui|
+      dapui.setup {
+        -- Set icons to characters that are more likely to work in every terminal.
+        --    Feel free to remove or use ones that you like more! :)
+        --    Don't feel like these are good choices.
+        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+        controls = {
+          icons = {
+            pause = '⏸',
+            play = '▶',
+            step_into = '⏎',
+            step_over = '⏭',
+            step_out = '⏮',
+            step_back = 'b',
+            run_last = '▶▶',
+            terminate = '⏹',
+            disconnect = '⏏',
+          },
+        },
+      }
+
+      -- Change breakpoint icons
+      vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
+      vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
+      local breakpoint_icons = vim.g.have_nerd_font
+          and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
+        or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
+      for type, icon in pairs(breakpoint_icons) do
+        local tp = 'Dap' .. type
+        local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
+        vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+      end
+
+      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+      dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+      -- Install golang specific config
+    end,
   },
   {
     'mfussenegger/nvim-dap-python',
@@ -1084,13 +1196,20 @@ require('lazy').setup({
       'nvim-neotest/nvim-nio',
     },
     config = function(_, opts)
-      local py = require 'dap-python'
-      local debugpy_path = require('mason-registry').get_package('debugpy'):get_install_path()
-      py.setup(debugpy_path .. '/venv/bin/python', opts)
-      py.test_runner = 'pytest'
+      local dap_python = require 'dap-python'
+      dap_python.setup('uv', opts)
+      dap_python.test_runner = 'pytest'
       local dap = require 'dap'
-      local configs = dap.configurations.python or {}
+      local configs = {}
       dap.configurations.python = configs
+      table.insert(configs, {
+        type = 'python',
+        request = 'launch',
+        name = 'Launch with uv',
+        program = '${file}',
+        python = 'uv run --env-file .env',
+        console = opts.console,
+      })
       table.insert(configs, {
         type = 'python',
         request = 'launch',
@@ -1111,15 +1230,15 @@ require('lazy').setup({
         console = opts.console,
         pythonPath = opts.pythonPath,
       })
-      table.insert(configs, {
-        type = 'python',
-        request = 'launch',
-        name = 'Launch main.py',
-        program = function()
-          return './main.py'
-        end,
-        pythonPath = opts.pythonPath,
-      })
+      -- table.insert(configs, {
+      --   type = 'python',
+      --   request = 'launch',
+      --   name = 'Launch main.py',
+      --   program = function()
+      --     return './main.py'
+      --   end,
+      --   pythonPath = opts.pythonPath,
+      -- })
       table.insert(configs, {
         type = 'python',
         request = 'launch',
@@ -1149,26 +1268,6 @@ require('lazy').setup({
         pythonPath = 'python',
         console = 'integratedTerminal',
       })
-    end,
-  },
-  {
-    'rcarriga/nvim-dap-ui',
-    dependencies = {
-      'mfussenegger/nvim-dap',
-    },
-    config = function()
-      local dap = require 'dap'
-      local dapui = require 'dapui'
-      dapui.setup()
-      dap.listeners.after.event_initialized['dapui_config'] = function()
-        dapui.open()
-      end
-      dap.listeners.after.event_terminated['dapui_config'] = function()
-        dapui.close()
-      end
-      dap.listeners.after.event_exited['dapui_config'] = function()
-        dapui.close()
-      end
     end,
   },
   {
