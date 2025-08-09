5939d582ceec84ad8c299bf282198a93010f0cd4
javlintor <jlinarestorres2@gmail.com>
2025-08-09 20:53:32 +0200

    migrate lualine to lazy config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,24 +1,22 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup {
-      options = {
-        theme = 'auto',
-      },
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch' },
-        lualine_c = {
-          {
-            'filename',
-            path = 1, -- 👈 Show relative path (use 2 for absolute path)
-          },
+  opts = {
+    options = {
+      theme = 'auto',
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch' },
+      lualine_c = {
+        {
+          'filename',
+          path = 1, -- 👈 Show relative path (use 2 for absolute path)
         },
-        lualine_x = { 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = {},
       },
-    }
-  end,
+      lualine_x = { 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = {},
+    },
+  },
 }
