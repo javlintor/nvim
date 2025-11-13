e337f1f8564902708d3fe512cfdf24ecacd0bb5e
Javier Linares <jlinarestorres2@gmail.com>
2025-11-13 08:43:09 +0100

    remove render markdown plugi

--- lua/plugins/render-markdown.lua
+++
@@ -1,6 +0,0 @@
-return {
-  enabled = false,
-  'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-  opts = {},
-}
