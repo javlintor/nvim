ada15a71cfc2bccb8c3dacdabba4b33ccbbe715b
javlintor <jlinarestorres2@gmail.com>
2025-12-03 11:20:51 +0100

    add copilot-chat

---
+++ lua/plugins/copilot-chat.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      -- See Configuration section for options
+    },
+    keys = {
+      { "<leader>zz", "<cmd>CopilotChatToggle<CR>",   desc = "[Z]copilot [Z]chat Toggle",   mode = "n" },
+      { "<leader>ze", "<cmd>CopilotChatExplain<CR>",  desc = "[Z]copilot [E]xplain",        mode = "v" },
+      { "<leader>zo", "<cmd>CopilotChatOptimize<CR>", desc = "[Z]copilot [O]ptimize",       mode = "v" },
+      { "<leader>zt", "<cmd>CopilotChatTests<CR>",    desc = "[Z]copilot generate [T]ests", mode = "v" }
+    }
+  },
+}
