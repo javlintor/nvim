5c3d0ff330171ac62ca72d2d808a151ca975ca91
javlintor <jlinarestorres2@gmail.com>
2026-01-14 19:40:57 +0100

    created a go to macro user command

--- lua/config/dbt.lua
+++ lua/config/dbt.lua
@@ -1,11 +1,16 @@
-local function dbt_goto_model()
+local function dbt_goto_relation(relation_type)
   local word = vim.fn.expand("<cword>")
   local cwd = vim.fn.getcwd()
-  local models_dir = cwd .. "/models"
+  local lookup_dir = nil
+  if relation_type == "model" then
+    lookup_dir = cwd .. "/models"
+  else
+    lookup_dir = cwd .. "/macros"
+  end
 
   -- Use grep to search recursively for the file
   -- The -l option lists only filenames that match
-  local handle = io.popen('grep -rl --include="*.sql" "^" "' .. models_dir .. '" | grep "/' .. word .. '\\.sql$"')
+  local handle = io.popen('grep -rl --include="*.sql" "^" "' .. lookup_dir .. '" | grep "/' .. word .. '\\.sql$"')
   if not handle then
     print("Failed to run grep")
     return
@@ -29,8 +34,18 @@ local function dbt_goto_model()
   vim.cmd("edit " .. result[1])
 end
 
+local function dbt_goto_model()
+  dbt_goto_relation("model")
+end
+
+local function dbt_goto_macro()
+  dbt_goto_relation("macro")
+end
+
 -- Create the command
 vim.api.nvim_create_user_command("DBTGoToModel", dbt_goto_model, {})
+vim.api.nvim_create_user_command("DBTGoToMacro", dbt_goto_macro, {})
 
 -- Optional keymap
 vim.api.nvim_set_keymap('n', '<leader>gm', ':DBTGoToModel<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gc', ':DBTGoToMacro<CR>', { noremap = true, silent = true })
