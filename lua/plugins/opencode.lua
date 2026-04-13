882e8d73420878ad52f962c3eb9aa852268a6032
Javier Linares <jlinarestorres2@gmail.com>
2026-04-13 08:59:46 +0200

    opencode plugin

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -33,11 +33,11 @@ return {
     vim.o.autoread = true -- Required for `opts.events.reload`
 
     -- Recommended/example keymaps
-    vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("", { submit = true }) end,
+    vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end,
       { desc = "Ask opencode…" })
-    vim.keymap.set({ "n", "x" }, "<leader>ox", function() require("opencode").select() end,
+    vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,
       { desc = "Execute opencode action…" })
-    vim.keymap.set({ "n", "t" }, "<leader>ot", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
+    vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
 
     vim.keymap.set({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end,
       { desc = "Add range to opencode", expr = true })
