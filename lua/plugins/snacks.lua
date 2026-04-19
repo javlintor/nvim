3e9a950a422c2a5981127b9158367f2010b5403d
Javier Linares <jlinarestorres2@gmail.com>
2026-04-19 21:19:08 +0200

    snacks plugins

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,11 +1,21 @@
 return {
   "folke/snacks.nvim",
   opts = {
-    terminal = {
-      enabled = true,
-    }
+    -- bigfile = { enabled = true },
+    -- dashboard = { enabled = true },
+    -- explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
   },
   init = function()
+    Snacks = require("snacks")
     vim.api.nvim_create_user_command("Term", function()
       Snacks.terminal.toggle()
     end, {})
