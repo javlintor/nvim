815887c47bc6fe4b1fe73ddc48b76b6ab911c015
Javier Linares <jlinarestorres2@gmail.com>
2026-04-19 21:18:31 +0200

    filter keymaps in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -41,9 +41,17 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>st', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader>ss', function()
+      builtin.buffers {
+        sort_mru = true,
+        ignore_current_buffer = true,
+      }
+    end, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>sn', function()
+      builtin.find_files { cwd = vim.fn.stdpath 'config' }
+    end, { desc = '[S]earch [N]eovim files' })
     -- search directories
     vim.keymap.set('n', '<leader>sD', function()
       builtin.find_files {
