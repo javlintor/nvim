82254d813d4d559cc315967469af0ed75ce25c83
javlintor <jlinarestorres2@gmail.com>
2026-04-20 09:54:58 +0200

    update lazygit with env variables

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,3 +1,5 @@
+vim.g.lazygit_floating_window_scaling_factor = 0.95
+
 return {
   "kdheepak/lazygit.nvim",
   lazy = true,
@@ -12,7 +14,14 @@ return {
     "nvim-lua/plenary.nvim",
   },
   keys = {
-    { "<leader>gg", "<cmd>LazyGit<cr>",                  desc = "LazyGit" },
+    {
+      "<leader>gg",
+      function()
+        vim.env.SKIP = 'install-dbt-deps'
+        vim.cmd('LazyGit')
+      end,
+      desc = "LazyGit"
+    },
     { "<leader>gf", "<cmd>LazyGitFilterCurrentFile<cr>", desc = "LazyGit" }
   }
 }
